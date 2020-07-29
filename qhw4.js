49fe32db4b267cc8d5d25f7d523b249b9875d620
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-07-28 23:55:41 -0500

    updated files

---
+++ qhw4.js
@@ -0,0 +1,32 @@
+var jsQuestions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Is JavaScript fun to work with?",
+    choices: ["No", "Sometimes", "What is Javascript?", "You mean Java?"],
+    answer: "Sometimes"
+  },
+  {
+    title: "DOM is an abreviation for ____",
+    choices: ["Data Object Mode", "Data Object Modification", "Document Object Model", "Data Object Machine"],
+    answer: "Document Object Model"
+  },
+  {
+    title: "JavaScript is Textile Mark Up (TML) version of Java?",
+    choices: ["True", "False"],
+    answer: "False"
+  },
+  {
+    title: "JavaScript is strongly typed language",
+    choices: ["True", "False"],
+    answer: "False"
+  }
+];
