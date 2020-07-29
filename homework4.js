ee4c0e213cc065129eb9524068421828f208ab05
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-07-28 22:06:38 -0500

    updated js

--- homework4.js
+++ homework4.js
@@ -4,55 +4,55 @@ var questions = [{
     question: "1. How do you write 'Hello World' in an alert box?",
     choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
     correctAnswer: 3
-}
+},
 
 {
     question: "2. How to empty an array in JavaScript?",
     choices: ["arrayList[]", "arrayList(0)", "arrayList.length=0", "arrayList.len(0)"],
     correctAnswer: 2
-}
+},
 
 {
     question: "3. What function to add an element at the begining of an array and one at the end?",
     choices: ["push,unshift", "unshift,push", "first,push", "unshift,last"],
     correctAnswer: 1
-}
+},
 
 {
     question: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
     choices: ["undefined", "0", "prints nothing", "Syntax error"],
     correctAnswer: 0
-}
+},
 
 {
     question: "5. What would following code return? console.log(typeof typeof 1);",
     choices: ["string", "number", "Syntax error", "undefined"],
     correctAnswer: 0
-}
+},
 
 {
 	question: "6. Which software company developed JavaScript?",
     choices: ["Mozilla", "Netscape", "Sun Microsystems", "Oracle"],
     correctAnswer: 1
-}
+},
 
 {
 	question: "7. What would be the result of 3+2+'7'?",
     choices: ["327", "12", "14", "57"],
     correctAnswer: 3
-}
+},
 
 {
 	question: "8. Look at the following selector: $('div'). What does it select?",
     choices: ["The first div element", "The last div element", "All div elements", "Current div element"],
     correctAnswer: 2
-}
+},
 
 {
 	question: "9. How can a value be appended to an array?",
     choices: ["arr(length).value;", "arr[arr.length]=value;", "arr[]=add(value);", "None of these"],
     correctAnswer: 1
-}
+},
 
 {
 	question: "10. What will the code below output to the console? console.log(1 +  +'2' + '2');",
@@ -206,17 +206,23 @@ function displayCurrentQuestion()
 {
 
     var currentQuestion = questions[currentQuestionIndex];
-    var titleEl = document.getElementById("question/title");
-    titleEl.textContent=currentQuestion.title
+    var titleEl = document.getElementById("questiontitle");
+    titleEl.textContent=currentQuestion.question
     currentChoices.innerHTML="";
 
+	var choiceList = document.getElementsByClassName("choiceList")
     currentQuestion.choices.forEach(function(choice, i){
         var choiceOptions = document.createElement("button");
         choiceOptions.setAttribute("class", "choice");
         choiceOptions.setAttribute("value", choice);
-        choiceOptions.textContent = i+1+". "+choice;
+		choiceOptions.textContent = i+1+". "+choice;
+		choiceOptions.addEventListener("click", checkAnswer)
+		choiceList[0].appendChild(choiceOptions)
+		
+
         //onclick function inside here
     });
+
 
         //WRITE FUNCTION THAT WILL LAY OUT WHT HAPPENS WHEN USER CLICKS RIGHT AND WRONG ANSWERS (TIME DEDUCTION)
         //A FUNCTION THAT SAYS WHAT HAPPENS WHEN THE QUIZ IS OVER
@@ -225,10 +231,10 @@ function displayCurrentQuestion()
 
 	if(c == 180) { c = 180; timedCount(); }
     //console.log("In display current Question");
-    var question = questions[currentQuestion].question;
+    var question = questions[currentQuestionIndex].question;
     var questionClass = $(document).find(".quizContainer > .question"); 
     var choiceList = $(document).find(".quizContainer > .choiceList");
-    var numChoices = questions[currentQuestion].choices.length;
+    var numChoices = questions[currentQuestionIndex].choices.length;
     // Set the questionClass text to the current question
     $(questionClass).text(question);
     // Remove all current <li> elements (if any)
@@ -238,7 +244,7 @@ function displayCurrentQuestion()
 	
     for (i = 0; i < numChoices; i++) 
 	{
-        choice = questions[currentQuestion].choices[i];
+        choice = questions[currentQuestionIndex].choices[i];
 		
 		if(iSelectedAnswer[currentQuestion] == i) {
 			$('<li><input type="radio" class="radio-inline" checked="checked"  value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
@@ -247,6 +253,10 @@ function displayCurrentQuestion()
 		}
     }
 }
+function checkAnswer(event)
+	{
+		console.log("click")
+	}
 
 function resetQuiz()
 {
@@ -292,9 +302,7 @@ function viewResults()
 		
 		if(iSelectedAnswer[currentQuestion] == i) {
 			if(questions[currentQuestion].correctAnswer == i) {
-                $('<li style="border:2px solid green; margin-top:10px;">
-                <input type="radio" class="radio-inline" checked="checked" value=' + i + ' name="dynradio" />
-                ' +  ' ' + choice  + '</li>').appendTo(choiceList);
+                $('<li style="border:2px solid green; margin-top:10px;"><input type="radio" class="radio-inline" checked="checked" value=' + i + ' name="dynradio" />' +  ' ' + choice  + '</li>').appendTo(choiceList);
 
             } 
             else {
